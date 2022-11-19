136f851c1f06f60d65d4557b43fd73e54b997f91
Cjreek <michael.morstein@gmail.com>
2022-11-19 23:47:06 +0100

    UI for Innate spell level, SpellGainTable, KnownSpellTable

---
+++ Usercontrols/SpellSlotTableComboBox.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Eos.Models;
+using Eos.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eos.Usercontrols
+{
+    /// <summary>
+    /// Interaktionslogik für SpellSlotTableComboBox.xaml
+    /// </summary>
+    public partial class SpellSlotTableComboBox : UserControl
+    {
+        public SpellSlotTableComboBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty SelectedValueProperty = DependencyProperty.Register("SelectedValue", typeof(SpellSlotTable), typeof(SpellSlotTableComboBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public static readonly DependencyProperty IsNullableProperty = DependencyProperty.Register("IsNullable", typeof(bool), typeof(SpellSlotTableComboBox), new PropertyMetadata(true));
+
+        public SpellSlotTable? SelectedValue
+        {
+            get { return (SpellSlotTable)GetValue(SelectedValueProperty); }
+            set { SetValue(SelectedValueProperty, value); }
+        }
+
+        public bool IsNullable
+        {
+            get { return (bool)GetValue(IsNullableProperty); }
+            set { SetValue(IsNullableProperty, value); }
+        }
+
+        private void btClear_Click(object sender, RoutedEventArgs e)
+        {
+            SetValue(SelectedValueProperty, null);
+        }
+    }
+}
