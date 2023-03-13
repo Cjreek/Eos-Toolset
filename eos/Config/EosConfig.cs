317ede17cf467a24256fc03d858754ddf4353f0e
Cjreek <michael.morstein@gmail.com>
2023-03-13 23:37:53 +0100

    Improved NWN Basepath detection for all platforms

--- eos/Config/EosConfig.cs
+++ eos/Config/EosConfig.cs
@@ -26,7 +26,7 @@ namespace Eos.Config
         public static String LastProject { get; set; } = "";
         public static TabLayout TabLayout { get; set; } = TabLayout.Multiline;
 
-        private static String FindNwnBasePath()
+        private static String FindSteamPathByRegistry()
         {
             // TODO: Linux/GoG/Beamdog/etc
             var nwnEEKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 704450", false);
@@ -34,6 +34,123 @@ namespace Eos.Config
                 return (String?)nwnEEKey.GetValue("InstallLocation") ?? "";
 
             return NwnBasePath;
+        }
+
+        private static String FindGOGPathByRegistry()
+        {
+            // TODO: Linux/GoG/Beamdog/etc
+            var nwnEEKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 704450", false);
+            if (nwnEEKey != null)
+                return (String?)nwnEEKey.GetValue("InstallLocation") ?? "";
+
+            return NwnBasePath;
+        }
+
+        private static String FindNwnBasePath()
+        {
+            string path;
+            
+            path = Environment.GetEnvironmentVariable("NWN_ROOT") ?? "";
+            if ((path != "") && (Directory.Exists(path))) return path;
+
+            // Steam
+            var steamPath = Path.Combine("Steam", "steamapps", "common", "Neverwinter Nights");
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                path = FindSteamPathByRegistry();
+                if (path == "")
+                    path = Path.Combine(Environment.GetEnvironmentVariable("PROGRAMFILES(X86)") ?? "", steamPath);
+
+                if (Directory.Exists(Path.Combine(path, "data")))
+                    return path;
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                var home = Environment.GetEnvironmentVariable("HOME") ?? "";
+                path = Path.Combine(home, ".local", "share", steamPath);
+
+                if (Directory.Exists(Path.Combine(path, "data")))
+                    return path;
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Other)
+            {
+                var home = Environment.GetEnvironmentVariable("HOME") ?? "";
+                path = Path.Combine(home, "Library", "Application Support", steamPath);
+
+                if (Directory.Exists(Path.Combine(path, "data")))
+                    return path;
+            }
+
+            // Beamdog
+            var beamdogSettings = Path.Combine("Beamdog Client", "settings.json");
+            var settingsFile = "";
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                var home = Environment.GetEnvironmentVariable("APPDATA") ?? "";
+                settingsFile = Path.Combine(home, beamdogSettings);
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                var home = Environment.GetEnvironmentVariable("HOME") ?? "";
+                settingsFile = Path.Combine(home, ".config", beamdogSettings);
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Other)
+            {
+                var home = Environment.GetEnvironmentVariable("HOME") ?? "";
+                settingsFile = Path.Combine(home, "Library", "Application Support", beamdogSettings);
+            }
+
+            if ((settingsFile != "") && (File.Exists(settingsFile)))
+            {
+                var fs = new FileStream(settingsFile, FileMode.Open, FileAccess.Read);
+                try
+                {
+                    if (JsonNode.Parse(fs) is JsonObject settingsRoot)
+                    {
+                        if (settingsRoot.ContainsKey("folders") && settingsRoot["folders"] is JsonArray folderArray)
+                        {
+                            foreach (var folder in folderArray)
+                            {
+                                if (folder == null) continue;
+                                var folderStr = folder.GetValue<String>() ?? "";
+
+                                var release00829 = Path.Combine(folderStr, "00829");
+                                if (Directory.Exists(Path.Combine(release00829, "data")))
+                                    return release00829;
+
+                                var release00785 = Path.Combine(folderStr, "00785");
+                                if (Directory.Exists(Path.Combine(release00785, "data")))
+                                    return release00785;
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    fs.Close();
+                }
+            }
+
+            // GOG
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                path = FindGOGPathByRegistry();
+                if (path == "")
+                    path = Path.Combine(Environment.GetEnvironmentVariable("PROGRAMFILES(X86)") ?? "", "GOG Galaxy", "Games", "Neverwinter Nights Enhanced Edition");
+
+                if (Directory.Exists(Path.Combine(path, "data")))
+                    return path;
+            }
+            else
+            {
+                var home = Environment.GetEnvironmentVariable("HOME") ?? "";
+                path = Path.Combine(home, "GOG Games", "Neverwinter Nights Enhanced Edition");
+
+                if (Directory.Exists(Path.Combine(path, "data")))
+                    return path;
+            }
+
+            return "";
         }
 
         public static void Load()
@@ -55,7 +172,6 @@ namespace Eos.Config
                     fs.Close();
                 }
             }
-
             if (NwnBasePath == String.Empty)
                 NwnBasePath = FindNwnBasePath();
         }
